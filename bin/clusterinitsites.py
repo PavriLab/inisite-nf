720342e18baa1e274d08754ceb89624d9c6b2bbb
daniel.malzl <daniel.malzl@imp.ac.at>
2020-03-30 17:53:19 +0200

    fixing subprocess call

--- bin/clusterinitsites.py
+++ bin/clusterinitsites.py
@@ -40,7 +40,8 @@ for group in bed.groupby('chr'):
 median = pd.Series(distances).median()
 logging.info('median distance between peaks is %d' % median)
 
-subprocess = sp.Popen('calling clusterscan.py clusterdist {0} {1} -a {2} -d {3} -n {4}'.format(
+logging.info('clustering initiation sites')
+subprocess = sp.Popen('clusterdist {0} {1} -a {2} -d {3} -n {4}'.format(
                             args.peakfile,
                             annotationFile,
                             args.outputPrefix,
